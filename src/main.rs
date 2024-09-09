8d9bc6ec8fa827ec94fd47f8a7b736f68f53eec8
iz7n <48165199+iz7n@users.noreply.github.com>
2024-09-08 22:11:39 -0400

    feat: show better error messages

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use std::{env, fs, io::Error, path::Path};
+use std::{env, fs, path::Path};
 use unicode_normalization::UnicodeNormalization;
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), String> {
     let args = env::args();
+    let args = args.skip(1);
+    if args.len() < 1 {
+        return Err("Expected at least one argument".into());
+    }
     for arg in args.skip(1) {
         let path = Path::new(&arg);
-        let half_width = fs::read_to_string(path).unwrap();
+        let half_width = match fs::read_to_string(path) {
+            Ok(content) => content,
+            Err(_) => return Err(format!("Failed to read from {}", path.display())),
+        };
 
         let full_width = half_width.chars().nfkc().stream_safe().collect::<String>();
-        fs::write(path, full_width)?;
+        if fs::write(path, full_width).is_err() {
+            return Err(format!("Failed to write to {}", path.display()));
+        }
     }
     Ok(())
 }
